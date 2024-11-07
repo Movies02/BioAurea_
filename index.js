f586cd3e2ebf5f06cf43e002eff218715672654b
JanilsonVaz <160428219+Movies02@users.noreply.github.com>
2024-11-07 17:22:01 -0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // index.js
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
 const fs = require('fs');
 
@@ -8,21 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'", "*"],  // Permite carregar recursos de qualquer origem
-        styleSrc: ["'self'", 'https:', "'unsafe-inline'"], // Permite estilos inline e externos
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "*"], // Permite qualquer script
-        mediaSrc: ["'self'", 'data:']
-    }
-}));
-app.use(helmet.crossOriginEmbedderPolicy());
-app.use(helmet.crossOriginOpenerPolicy());
-app.use(helmet.crossOriginResourcePolicy({ policy: "same-origin" }));
-app.use(helmet.dnsPrefetchControl({ allow: false }));
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.hsts({ maxAge: 31536000, includeSubDomains: true }));
-app.use(helmet.xssFilter());
 
 app.get('/dados', (req, res) => {
     fs.readFile('produtos.json', "utf-8", (err, data) => {

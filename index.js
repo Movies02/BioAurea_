4fd15993f01e8c1853776d1ecd1006c3ca4b030e
JanilsonVaz <160428219+Movies02@users.noreply.github.com>
2024-11-07 17:37:58 -0100

    atualizando

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const fs = require('fs');
 
@@ -7,6 +8,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", 'https:'],
+        scriptSrc: ["'self'", 'https:']
+    }
+}));
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy({ policy: "same-origin" }));
+app.use(helmet.dnsPrefetchControl({ allow: false }));
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.hsts({ maxAge: 31536000, includeSubDomains: true }));
+app.use(helmet.xssFilter());
 
 app.get('/dados', (req, res) => {
     fs.readFile('produtos.json', "utf-8", (err, data) => {
